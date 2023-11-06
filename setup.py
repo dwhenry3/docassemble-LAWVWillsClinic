435abc892176dbc0f0a2c9a686e5cc15e93c90e3
System Administrator <127511792+dwhenry3@users.noreply.github.com>
2023-11-06 16:48:40 -0500

    final commit before launch.  Updated template to improve readability, updated code to carryover full name (including middle)

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ def find_package_data(where='.', package='', exclude=standard_exclude, exclude_d
     return out
 
 setup(name='docassemble.LAWVWillsClinic',
-      version='0.0.8',
+      version='1.0.0',
       description=('A docassemble extension for creating a basic will during a LAWV Will Clinic'),
       long_description='A guided interview for creating a Last Will and Testatment for a LAWV Will Clinic.  The interview assumes that it is going to be completed by an attorney or paralegal assisting in the clinic after the client has already submitted a Will Clinic questionnaire.  The interview seeks biographical information about the testator and family.  It then seeks specific gifts, general gifts, residual assignees, executors, and guardians. The interview produces a Word document to allow for final edits.',
       long_description_content_type='text/markdown',
